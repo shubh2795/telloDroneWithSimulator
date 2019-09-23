666cc931a4aedfc3f182b0f9f38e27ce7dfb8da6
Shubham Swami <shubhamswami2795@gmail.com>
2019-09-23 10:36:30 -0600

    Adding UI

---
+++ telloDrone/src/main/java/TelloGUI.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class TelloGUI {
+    private JPanel TelloDroneGUI;
+    private JTextField DroneStatusText;
+    private JTextField BatteryStatusText;
+    private JTextField DroneSpeedText;
+    private JTextField FlightTimeText;
+
+
+
+    public TelloGUI() {
+
+        DroneStatusText.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        FlightTimeText.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        BatteryStatusText.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        DroneSpeedText.addPropertyChangeListener(new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt) {
+
+            }
+        });
+    }
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("TelloGUI");
+        frame.setContentPane(new TelloGUI().TelloDroneGUI);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
--- telloDrone/src/main/java/UserInterface.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class UserInterface {
-    private JPanel panel1;
-    private JTextField batteryTextField;
-    private JTextField flightTimeTextField;
-    private JTextField speedTextField;
-    private JProgressBar progressBar1;
-}
